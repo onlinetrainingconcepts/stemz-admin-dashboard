2f17eccfa73a07cbafd60b728222b1c26aeeb3fe
Jordan Hunnicutt <jordanneilhunnicutt@gmail.com>
2023-10-26 17:20:03 -0400

    fix: update js to accommodate buttons with no accordion conten
    This commit changes the fundamental behavior of the nav buttons. Previously, accordions could be toggled on or off independently. Now, only one button can be "active," and any other open accordions will be closed.

--- main.js
+++ main.js
@@ -32,14 +32,57 @@ class AdminDashboard {
 	}
 
 	navButtonHandler(button) {
-		const accordionContent = button.nextElementSibling;
+		let accordionContent;
+		let tabNumber;
+		let tabToActivate;
 
-		button.classList.toggle('active');
-
-		if (button.classList.contains('active')) {
-			this.addClass(accordionContent, 'active');
+		// varifies sibiling is accordion content before assigning variable
+		if (button.nextElementSibling.classList.contains('accordion-content')) {
+			accordionContent = button.nextElementSibling;
 		} else {
-			this.removeClass(accordionContent, 'active');
+			accordionContent = null;
+		}
+
+		// verifies button contains property before assigning variable
+		if (button.dataset.forTab) {
+			tabNumber = button.dataset.forTab;
+		} else {
+			tabNumber = null;
+		}
+
+		// removes active class for all buttons and accordion content
+		this.removeClassAll(this.nav, '.dashboard__button', 'active');
+		this.removeClassAll(this.nav, '.accordion-content', 'active');
+
+		// adds active class for current button
+		this.addClass(button, 'active');
+
+		// if button has accordion items, do the following
+		if (accordionContent) {
+			const firstButton = accordionContent.querySelector('.dashboard__button');
+			// adds active class for accordion content
+			this.addClass(accordionContent, 'active');
+
+			// clicks first accordion button in the series
+			firstButton.click();
+		}
+
+		// if button does not have accordion items, do the following
+		if (tabNumber) {
+			// assigns value to variable
+			tabToActivate = this.contentContainer.querySelector(
+				`.tabcontent[data-tab="${tabNumber}"]`
+			);
+
+			// removes active classes for all tab content
+			this.removeClassAll(
+				this.contentContainer,
+				'.tabcontent',
+				'tabcontent--active'
+			);
+
+			// adds active class to current tab content
+			this.addClass(tabToActivate, 'tabcontent--active');
 		}
 	}
 
@@ -49,12 +92,12 @@ class AdminDashboard {
 			`.tabcontent[data-tab="${tabNumber}"]`
 		);
 		this.removeClassAll(this.nav, '.dashboard__button.sub', 'active');
-		this.addClass(button, 'active');
 		this.removeClassAll(
 			this.contentContainer,
 			'.tabcontent',
 			'tabcontent--active'
 		);
+		this.addClass(button, 'active');
 		this.addClass(tabToActivate, 'tabcontent--active');
 	}
 
@@ -73,9 +116,6 @@ class AdminDashboard {
 				this.accordionButtonHandler(button);
 			});
 		});
-
-		// ensures the first accorion button shows by default
-		accordionBtns[0].click();
 	}
 }
 
